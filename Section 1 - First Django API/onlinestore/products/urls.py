34f7dc59719839e0915ea77ecc89814c325014c9
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-11-18 09:32:47 +0100

    Completed competency assessment (Section 1)

--- Section 1 - First Django API/onlinestore/products/urls.py
+++ Section 1 - First Django API/onlinestore/products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import product_list, product_detail
+from .views import product_list, product_detail, manufacturer_list, manufacturer_detail
 # from .views import ProductDetailView, ProductListView
 
 
@@ -7,5 +7,8 @@ urlpatterns = [
     # path("", ProductListView.as_view(), name="product-list"),
     # path("products/<int:pk>/", ProductDetailView.as_view(), name="product-detail")
     path("products/", product_list, name="product-list"),
-    path("products/<int:pk>/", product_detail, name="product-detail")
+    path("products/<int:pk>/", product_detail, name="product-detail"),
+
+    path("manufacturers/", manufacturer_list, name="manufacturer-list"),
+    path("manufacturers/<int:pk>/", manufacturer_detail, name="manufacturer-detail"),
 ]
--- Section 1 - First Django API/onlinestore/products/views.py
+++ Section 1 - First Django API/onlinestore/products/views.py
@@ -40,7 +40,39 @@ def product_detail(request, pk):
         response = JsonResponse({
             "error": {
                 "code": 404,
-                "message": "product not found!"
+                "message": "Product not found!"
+            }
+        }, status=404)
+
+    return response
+
+
+def manufacturer_list(request):
+    manufacturers = Manufacturer.objects.filter(active=True)
+    data = {"manufacturers": list(manufacturers.values("pk", "name"))}
+    response = JsonResponse(data)
+
+    return response
+
+
+def manufacturer_detail(request, pk):
+    try:
+        manufacturer = Manufacturer.objects.get(pk=pk)
+        manufacturer_products = manufacturer.products.all()
+
+        data = {
+            "manufacturer": {
+                "name": manufacturer.name,
+                "manufacturer": manufacturer.location,
+                "active": manufacturer.active,
+                "products": list(manufacturer_products.values())
+        }}
+        response = JsonResponse(data)
+    except Product.DoesNotExist:
+        response = JsonResponse({
+            "error": {
+                "code": 404,
+                "message": "Manufacturer not found!"
             }
         }, status=404)
 
